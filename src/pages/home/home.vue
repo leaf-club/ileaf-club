635eacd8a7ec35251cd2d421f238f718efe07a52
Yann001 <1156203992@qq.com>
2017-10-28 20:27:20 +0800

    init project

---
+++ src/pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'This is home page'
+      };
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,56 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
+
+const home = r => require.ensure([], () => r(require('../pages/home/home')), 'home');
+const article = r => require.ensure([], () => r(require('../pages/article/article')), 'article');
+const work = r => require.ensure([], () => r(require('../pages/work/work')), 'work');
+const read = r => require.ensure([], () => r(require('../pages/read/read')), 'read');
+const edit = r => require.ensure([], () => r(require('../pages/edit/edit')), 'edit');
+const about = r => require.ensure([], () => r(require('../pages/about/about')), 'about');
+const login = r => require.ensure([], () => r(require('../pages/login/login')), 'login');
+const personal = r => require.ensure([], () => r(require('../pages/personal/personal')), 'personal');
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: HelloWorld
+    },
+    {
+      path: '/home',
+      component: home
+    },
+    {
+      path: '/article',
+      component: article
+    },
+    {
+      path: '/read',
+      component: read
+    },
+    {
+      path: '/eidt',
+      component: edit
+    },
+    {
+      path: '/work',
+      component: work
+    },
+    {
+      path: '/about',
+      component: about
+    },
+    {
+      path: '/login',
+      component: login
+    },
+    {
+      path: '/personal',
+      component: personal
+    }
+  ]
+});
