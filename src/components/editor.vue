c9234d4085ac827537e01f2f94121857927c6942
Yann001 <1156203992@qq.com>
2017-10-30 19:49:48 +0800

    finish base editor component

---
+++ src/components/editor.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <mavon-editor :ishljs="true" style="height: 100%"></mavon-editor>
+  </div>
+</template>
+
+<script>
+import { mavonEditor } from 'mavon-editor';
+import 'mavon-editor/dist/css/index.css';
+export default {
+  name: 'editor',
+  data () {
+    return {};
+  },
+  components: {
+    mavonEditor
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/edit/edit.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="edit">
+    <head-top :is-login="true"></head-top>
+    <editor class="editor"></editor>
+  </div>
+</template>
+
+<script>
+import headTop from '@/components/header';
+import editor from '@/components/editor';
+export default {
+  data () {
+    return {};
+  },
+  components: {
+    headTop,
+    editor
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .editor {
+    position: relative;
+    top: 80px;
+    height: 600px;
+  }
+</style>
+
