580ef19e2d4f33f0735e81bee034a8721c027981
Yann001 <1156203992@qq.com>
2017-11-06 17:16:42 +0800

    # This is a combination of 2 commits. # This is the 1st commit message:
    clean up workbase
    
    # This is the commit message #2:
    
    rem更改

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,43 @@
 <template>
   <div id="app">
+<<<<<<< HEAD
     <router-view/>
+=======
+    <head-top></head-top>
+    <article>
+      <router-view></router-view>
+    </article>
+    <footer></footer>
+>>>>>>> 318cca9... header，app
   </div>
 </template>
 
 <script>
+import headTop from './components/header.vue';
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    headTop
+  }
 };
 </script>
 
 <style>
-/* @import "../assets/css/normalize.css"; */
+@import "/static/normalize.css";
 html,
 body {
   margin: 0;
   padding: 0;
   background-color: #ebeef5;
   font-family: 'Microsoft YaHei', Helvetica, Arial, sans-serif;
-  font-size: 16px;
+  font-size: 100px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+#app>article {
+  margin: 0.6rem auto 0;
+  max-width: 8rem;
+  background-color: #fff;
+  font-size: 0.16rem;
 }
 </style>
