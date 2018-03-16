8a2fece2a8045b182c4b019e91676cf70a5e444b
Chunliuliu <1009260707@qq.com>
2018-03-16 20:14:04 +0800

    设置页面最大最小宽度（yxy提交）

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,52 +1,54 @@
 <template>
   <div>
     <header>
-      <nav>
-        <router-link to="/" class="logo">
-          <img src="../assets/images/logo.png" width="150" alt="logo-picture">
-        </router-link>
-        <ul>
-          <li><router-link to="/home">首页</router-link></li>
-          <li><router-link to="/article">博文</router-link></li>
-          <li><router-link to="/work">作品</router-link></li>
-          <li><router-link to="/about">关于</router-link></li>
-        </ul>
-      </nav>
-      <ul id="options">
-        <li v-if="isLogin" class="user-info">
-          <a href="javascript:void(0)" class="avatar" :style="'background-image: url('+userInfo.avatar+');'">
-            <!-- <img :src="userInfo.avatar" width="40" alt="用户头像"> -->
-          </a>
-          <ul class="drop-menu">
-            <li class="item">
-              <router-link :to="'/personal?id='+userInfo.userId" class="link">我的主页</router-link>
-            </li>
-            <li class="item">
-              <router-link to="/personal" class="link">我的收藏</router-link>
-            </li>
-            <li class="item">
-              <router-link to="/edit" class="link">发布文章</router-link>
-            </li>
-            <li class="item">
-              <router-link to="/work" class="link">发布作品</router-link>
-            </li>
-            <li class="item">
-              <router-link to="/home" class="link">设置</router-link>
-            </li>
-            <li class="item">
-              <a href="javascript:void(0)" class="link" @click="logout">退出</a>
-              <!-- <router-link to="/home" class="link">退出</router-link> -->
-            </li>
+      <section class="header-content">
+        <nav>
+          <router-link to="/" class="logo">
+            <img src="../assets/images/logo.png" width="150" alt="logo-picture">
+          </router-link>
+          <ul>
+            <li><router-link to="/home">首页</router-link></li>
+            <li><router-link to="/article">博文</router-link></li>
+            <li><router-link to="/work">作品</router-link></li>
+            <li><router-link to="/about">关于</router-link></li>
           </ul>
-        </li>
-        <li v-if="!isLogin" class="login">
-          <span class="btn-login" @click="login">登录</span>
-          <span class="btn-register" @click="register">注册</span>
-        </li>
-        <li class="write">
-          <router-link to="/edit" class="btn-write">写文章</router-link>
-        </li>
-      </ul>
+        </nav>
+        <ul id="options">
+          <li v-if="isLogin" class="user-info">
+            <a href="javascript:void(0)" class="avatar" :style="'background-image: url('+userInfo.avatar+');'">
+              <!-- <img :src="userInfo.avatar" width="40" alt="用户头像"> -->
+            </a>
+            <ul class="drop-menu">
+              <li class="item">
+                <router-link :to="'/personal?id='+userInfo.userId" class="link">我的主页</router-link>
+              </li>
+              <li class="item">
+                <router-link to="/personal" class="link">我的收藏</router-link>
+              </li>
+              <li class="item">
+                <router-link to="/edit" class="link">发布文章</router-link>
+              </li>
+              <li class="item">
+                <router-link to="/work" class="link">发布作品</router-link>
+              </li>
+              <li class="item">
+                <router-link to="/home" class="link">设置</router-link>
+              </li>
+              <li class="item">
+                <a href="javascript:void(0)" class="link" @click="logout">退出</a>
+                <!-- <router-link to="/home" class="link">退出</router-link> -->
+              </li>
+            </ul>
+          </li>
+          <li v-if="!isLogin" class="login">
+            <span class="btn-login" @click="login">登录</span>
+            <span class="btn-register" @click="register">注册</span>
+          </li>
+          <li class="write">
+            <router-link to="/edit" class="btn-write">写文章</router-link>
+          </li>
+        </ul>
+      </section>
     </header>
     <section :class="doLogin ? 'login-wrap' : 'login-wrap register-wrap'" v-if="doLogin || doRegister">
       <form class="login-box">
@@ -187,27 +189,51 @@
     height: 0.60rem;
     position: fixed;
     top: 0;
-    display: flex;
-    justify-content: space-between;
     background: #fff;
     z-index: 999999;
-    nav, #options {
+    min-width: 9rem;
+    .header-content{
+      max-width: 13.5rem;
+      height: .6rem;
+      margin: 0 auto;
       display: flex;
-      font-size: 0.16rem;
-    }
-    nav {
-      margin-left: 0.20rem;
-      .logo {
+      justify-content: space-between;
+      nav, #options {
         display: flex;
-        align-items: center;
+        font-size: 0.16rem;
       }
-      ul {
-        margin: 0;
-        padding-left: 0.20rem;
-        display: flex;
+      nav {
+        margin-left: 0.20rem;
+        .logo {
+          display: flex;
+          align-items: center;
+        }
+        ul {
+          margin: 0;
+          padding-left: 0.20rem;
+          display: flex;
+          align-items: center;
+          list-style: none;
+          li{
+            width: 0.50rem;
+            text-align: center;
+            margin-right: 0.20rem;
+            a{
+              color: $mainFontColor;
+              text-decoration: none;
+            }
+            a.router-link-active, a:hover{
+              color: $mainColor;
+            }
+          }
+        }
+      }
+      #options {
+        margin-right: 0.20rem;
+        padding: 0 0.20rem 0 0;
         align-items: center;
         list-style: none;
-        li{
+        li {
           width: 0.50rem;
           text-align: center;
           margin-right: 0.20rem;
@@ -215,95 +241,77 @@
             color: $mainFontColor;
             text-decoration: none;
           }
-          a.router-link-active, a:hover{
+          a.router-link-active, a:hover {
             color: $mainColor;
           }
-        }
-      }
-    }
-    #options {
-      margin-right: 0.20rem;
-      padding: 0 0.20rem 0 0;
-      align-items: center;
-      list-style: none;
-      li {
-        width: 0.50rem;
-        text-align: center;
-        margin-right: 0.20rem;
-        a{
-          color: $mainFontColor;
-          text-decoration: none;
-        }
-        a.router-link-active, a:hover {
-          color: $mainColor;
-        }
-        span {
-          color: #999;
-          cursor: pointer;
-        }
-        span:hover {
-          color: #4eb2a3;
-        }
-        span.btn-login:after {
-          content: "\B7";
-          margin: 0 .04rem 0 .07rem;
-        }
-        .btn-write {
-          color: #fff;
-          background-color: $mainColor;
-          border-radius: 0.07rem;
-          display: inline-block;
-          width: 0.80rem;
-          height: 0.35rem;
-          line-height: 0.35rem;
-          &:hover {
-            background-color: $hoverColor;
+          span {
+            color: #999;
+            cursor: pointer;
+          }
+          span:hover {
+            color: #4eb2a3;
+          }
+          span.btn-login:after {
+            content: "\B7";
+            margin: 0 .04rem 0 .07rem;
+          }
+          .btn-write {
             color: #fff;
-          }
-        }
-      }
-      li.login {
-        width: 1.2rem
-      }
-      li.write {
-        a.btn-write {
-          color: white;
-        }
-      }
-      .user-info {
-        height: 0.60rem;
-        display: flex;
-        align-items: center;
-        width: 0.40rem;
-        &:hover {
-          ul {
-            display: block;
-          }
-        }
-        .avatar {
-          width: 0.40rem;
-          height: 0.40rem;
-          display: inline-block;
-          border-radius: .2rem;
-          background-size:cover;
-        }
-        ul {
-          display: none;
-          position: absolute;
-          top: 100%;
-          margin-top: -0.02rem;
-          padding: 0 0.10rem;
-          list-style: none;
-          background-color: #fff;
-          background-clip: padding-box;
-          border: 0.01rem solid rgba(0, 0, 0, -0.85);
-          border-radius: 0.01rem;
-          -webkit-box-shadow: 0 0.06rem 0.12rem rgba(0, 0, 0, 0.175);
-          box-shadow: 0 0.03rem 0.12rem rgba(0, 0, 0, 0.175);
-          li {
+            background-color: $mainColor;
+            border-radius: 0.07rem;
+            display: inline-block;
             width: 0.80rem;
-            margin: 0.10rem 0.05rem;
-            text-align: left;
+            height: 0.35rem;
+            line-height: 0.35rem;
+            &:hover {
+              background-color: $hoverColor;
+              color: #fff;
+            }
+          }
+        }
+        li.login {
+          width: 1.2rem
+        }
+        li.write {
+          a.btn-write {
+            color: white;
+          }
+        }
+        .user-info {
+          height: 0.60rem;
+          display: flex;
+          align-items: center;
+          width: 0.40rem;
+          &:hover {
+            ul {
+              display: block;
+            }
+          }
+          .avatar {
+            width: 0.40rem;
+            height: 0.40rem;
+            display: inline-block;
+            border-radius: .2rem;
+            background-size:cover;
+          }
+          ul {
+            display: none;
+            position: absolute;
+            top: 100%;
+            margin-top: -0.02rem;
+            padding: 0 0.10rem;
+            list-style: none;
+            background-color: #fff;
+            background-clip: padding-box;
+            border: 0.01rem solid rgba(0, 0, 0, -0.85);
+            border-radius: 0.01rem;
+            -webkit-box-shadow: 0 0.06rem 0.12rem rgba(0, 0, 0, 0.175);
+            box-shadow: 0 0.03rem 0.12rem rgba(0, 0, 0, 0.175);
+            li {
+              width: 0.80rem;
+              margin: 0.10rem 0.05rem;
+              text-align: left;
+            }
           }
         }
       }
